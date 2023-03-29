304b8a864526b4e918d304cc44d50d90c1ebb7b9
Luffy Dinh <luffy.dinh@zillearn.com>
2023-03-29 09:36:51 +0700

    Initial version of car_maintainance generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/luffy/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.luffy.repository;
+
+import com.luffy.domain.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the {@link User} entity.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findOneByLogin(String login);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByLogin(String login);
+
+    Page<User> findAllByIdNotNullAndActivatedIsTrue(Pageable pageable);
+}
---
+++ src/main/java/com/luffy/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.luffy.service.dto;
